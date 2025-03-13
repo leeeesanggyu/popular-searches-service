6206c0c0597d9881835265d9bf4a39e94b0c79db
이상규 <76906458+leeeesanggyu@users.noreply.github.com>
2025-03-13 16:56:11 +0900

    setting configuration module

---
+++ configuration/src/main/java/com/salgu/PopularSearchService.java
@@ -0,0 +1,11 @@
+package com.salgu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopularSearchService {
+    public static void main(String[] args) {
+        SpringApplication.run(PopularSearchService.class, args);
+    }
+}
