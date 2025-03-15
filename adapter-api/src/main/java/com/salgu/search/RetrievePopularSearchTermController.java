f1b4ac6327f86cc9becd4a3bdbff849c753284db
이상규 <76906458+leeeesanggyu@users.noreply.github.com>
2025-03-16 02:29:24 +0900

    #1 DB로 인기 검색어 순위 조회 Controller 구현

---
+++ adapter-api/src/main/java/com/salgu/search/RetrievePopularSearchTermController.java
@@ -0,0 +1,30 @@
+package com.salgu.search;
+
+import com.salgu.search.in.RetrievePopularSearchTermQuery;
+import com.salgu.search.in.RetrievePopularSearchTermUseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class RetrievePopularSearchTermController {
+
+    private final RetrievePopularSearchTermUseCase retrievePopularSearchTermUseCase;
+
+    @GetMapping("/search-terms")
+    public List<String> retrieve(@RequestParam int limit) {
+        RetrievePopularSearchTermQuery query = new RetrievePopularSearchTermQuery(limit);
+
+        SearchTermCollection searchTerms = retrievePopularSearchTermUseCase.retrieve(query);
+
+        return searchTerms.getSearchTerms().stream()
+                .map(SearchTerm::getKeyword)
+                .toList();
+    }
+}
