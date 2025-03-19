d0c9940fb44752333192a1f4fc0e27f2ee5d56b5
이상규 <76906458+leeeesanggyu@users.noreply.github.com>
2025-03-20 00:33:48 +0900

    #2 adapter-persistence-ranking에 레디스 연동

---
+++ adapter-persistence-ranking/src/main/java/com/salgu/configuration/RedisConfiguration.java
@@ -0,0 +1,26 @@
+package com.salgu.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfiguration {
+
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 6379;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(HOST, PORT);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
