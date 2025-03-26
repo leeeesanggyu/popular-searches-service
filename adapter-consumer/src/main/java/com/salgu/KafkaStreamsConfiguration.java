1cbe86506d2ec5f4ea5d8aad6dac79a67f4abf53
이상규 <76906458+leeeesanggyu@users.noreply.github.com>
2025-03-26 14:50:39 +0900

    #5 KafkaStreamsConfiguration 분리

---
+++ adapter-consumer/src/main/java/com/salgu/KafkaStreamsConfiguration.java
@@ -0,0 +1,9 @@
+package com.salgu;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+
+@Configuration
+@EnableKafkaStreams
+public class KafkaStreamsConfiguration {
+}
